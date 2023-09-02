c07b9471a2edb4e64673520f77422e5fc249e6f5
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2023-09-02 23:07:49 +0200

    task2-finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const onPlay = throttle(data => {
+  console.log(data.seconds);
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}, 1000);
+player.on('timeupdate', onPlay);
+
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+player.setCurrentTime(currentTime);
