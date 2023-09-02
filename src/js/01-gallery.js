6cbc4d83c07ae7ad63959a5f00db0c558fc69887
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2023-09-02 22:51:06 +0200

    finish-task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const imgList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`;
+  })
+  .join('');
+
+imgList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+imgList.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target === e.currentTarget) {
+    return;
+  }
+
+  galleryItems.find(
+    ({ description }) => description === e.target.getAttribute('alt')
+  );
+  new SimpleLightbox('.gallery__item a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+});
