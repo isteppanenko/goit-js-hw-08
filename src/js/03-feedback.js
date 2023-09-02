03fa2c17cab8446c9fe98db6e4e6fa86f6956364
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2023-09-02 23:11:33 +0200

    task3-finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+const obj = {};
+const inputHandler = throttle(event => {
+  event.preventDefault();
+  obj.email = form.elements[0].value;
+  obj.message = form.elements[1].value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+}, 500);
+
+form.addEventListener('input', inputHandler);
+
+const localObj = JSON.parse(localStorage.getItem('feedback-form-state')) ?? '';
+
+if (localObj !== '') {
+  form.elements[0].value = localObj.email;
+  form.elements[1].value = localObj.message;
+}
+
+const btnHandler = event => {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+form.addEventListener('submit', btnHandler);
